a298615f4ec19ca79c422bf3447cbadf46dc6c4f
robertoshimizu <rshimizubr@gmail.com>
2022-09-19 16:54:25 -0300

    define TokenService

--- src/main/java/com/example/alura_springrestapi/controller/AuthenticationController.java
+++ src/main/java/com/example/alura_springrestapi/controller/AuthenticationController.java
@@ -1,7 +1,12 @@
 package com.example.alura_springrestapi.controller;
 
 import com.example.alura_springrestapi.controller.form.LoginForm;
+import com.example.alura_springrestapi.secutity.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -10,13 +15,26 @@ import javax.validation.Valid;
 @RequestMapping("/auth")
 public class AuthenticationController {
 
+    @Autowired
+    private AuthenticationManager authManager;
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form) {
 
-        System.out.println(form.getEmail());
-        System.out.println(form.getSenha());
+        UsernamePasswordAuthenticationToken dadosLogin = form.converter();
 
-        return ResponseEntity.ok().build();
+        try {
+            Authentication authentication = authManager.authenticate(dadosLogin);
+
+            String token = tokenService.gerarToken(authentication);
+            System.out.println(token);
+            return ResponseEntity.ok().build();
+        } catch(Exception e)  {
+            return ResponseEntity.badRequest().build();
+        }
+
 
     }
 }
--- src/main/java/com/example/alura_springrestapi/controller/form/LoginForm.java
+++ src/main/java/com/example/alura_springrestapi/controller/form/LoginForm.java
@@ -1,5 +1,7 @@
 package com.example.alura_springrestapi.controller.form;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 public class LoginForm {
     private String email;
     private String senha;
@@ -23,5 +25,9 @@ public class LoginForm {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public UsernamePasswordAuthenticationToken converter() {
+        return  new UsernamePasswordAuthenticationToken(email, senha);
     }
 }
